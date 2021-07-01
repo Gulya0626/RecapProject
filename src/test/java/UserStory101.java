e4ca69278b7715d5b5c96592b6c84899720d2c34
Gulya0626 <gulyakg@yahoo.com>
2021-06-30 19:25:27 -0500

    Ready to push to remote

--- src/test/java/UserStory101.java
+++ src/test/java/UserStory101.java
@@ -2,5 +2,6 @@ public class UserStory101 {
     public static void main(String[] args) {
         System.out.println("User Story 101");
         // user story 101 has began
+        // ready to push to remote
     }
 }
